61e56605c44a7cb48d14530524f8d8f0ff0ee8cd
Eder Canada <canada.eder@gmail.com>
2021-10-17 10:56:15 -0700

    hooks accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,23 +1,32 @@
 import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
   const onTitleClick = (index) => {
-    console.log('title click ', index);
+    setActiveIndex(index);
   };
+
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick('click', index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"> </i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p> {item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+    </div>
+  );
 };
 
 export default Accordion;
