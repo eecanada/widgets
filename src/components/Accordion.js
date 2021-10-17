d5551a82e7071de752578822e06636fa5bec10c4
Eder Canada <canada.eder@gmail.com>
2021-10-16 19:28:12 -0700

    refactor onClick

---
+++ src/components/Accordion.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Accordion = ({ items }) => {
+  const onTitleClick = (index) => {
+    console.log('title click ', index);
+  };
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active" onClick={() => onTitleClick('click', index)}>
+          <i className="dropdown icon"> </i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p> {item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
+
+export default Accordion;
