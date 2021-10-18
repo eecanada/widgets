2c99757f2e8aec9b203316f152dcdf6be4a39a25
Eder Canada <canada.eder@gmail.com>
2021-10-18 16:53:30 -0700

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
@@ -17,12 +19,31 @@ const items = [
   },
 ];
 
+const options = [
+  { label: 'The Color Red', value: 'red' },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue',
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       {/* <Accordion items={items} /> */}
 
-      <Search />
+      {/* <Search /> */}
+
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
