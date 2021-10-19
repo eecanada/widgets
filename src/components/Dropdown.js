a8efb6b11eac45cc713d1bc29a73272b4d74e572
Eder Canada <canada.eder@gmail.com>
2021-10-18 18:51:19 -0700

    toggle dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -6,18 +6,20 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef();
 
-
   useEffect(() => {
-    document.body.addEventListener(
-      'click',
-      (event) => {
-        if(ref.current.contains(event.target)){
-          return; 
-        }
-        setOpen(false);
-      },
-      { capture: true }
-    );
+    const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener('click', onBodyClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener('click', onBodyClick, {
+        capture: true,
+      });
+    };
   }, []);
 
   const renderedOptions = options.map((option) => {
@@ -36,7 +38,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     );
   });
 
-  console.log(ref.current)
+  console.log(ref.current);
   return (
     <div ref={ref} className="ui form">
       <div className="field">
