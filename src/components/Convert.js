8ba2b7ab7ddcea144a4e22b981b69da28a819fb9
Eder Canada <canada.eder@gmail.com>
2021-10-20 10:33:31 -0700

    successful request and response

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,10 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { use } from 'react/cjs/react.development';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
+
   useEffect(() => {
-    console.log('New language or text');
-  }, [language, text]);
-  return <div> Convert</div>;
+    const timerid = setTimeout(() => {
+      setDebouncedText(text);
+    }, 1000);
+    return () => {
+      clearTimeout(timerid);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: API_KEY,
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
+  }, [language, debouncedText]);
+  return <div>{translated}</div>;
 };
 
 export default Convert;
